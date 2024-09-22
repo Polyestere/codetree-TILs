f657739e0f21b147a85836883c8580bb8fd0f374
Polyestere <83736286+Polyestere@users.noreply.github.com>
2024-09-22 20:59:23 +0900

    Title: 윤년 횟수 구하기, Time: 125ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240922/윤년 횟수 구하기/number-of-leap-years.py
+++ 240922/윤년 횟수 구하기/number-of-leap-years.py
@@ -1,7 +1,8 @@
 n = int(input())
 yoon_cnt = 0
 for i in range(1,n):
-    if i % 100 == 0 and i % 400 == 1:
+    if i % 100 == 0 and i % 400 != 0:
+        i += 1
         continue
     elif i % 4 == 0:
         yoon_cnt += 1
