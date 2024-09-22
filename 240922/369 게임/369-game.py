386de57a28fc0ac7b20900ddab6a3d03732cea20
Polyestere <83736286+Polyestere@users.noreply.github.com>
2024-09-22 20:07:06 +0900

    Title: 369 게임, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240922/369 게임/369-game.py
+++ 240922/369 게임/369-game.py
@@ -1,10 +1,11 @@
 n = int(input())
 numbers = list(range(1,n+1))
+
 for i in range(len(numbers)):
     if numbers[i] % 3 == 0: #3의 배수인가
         numbers[i] = 0
-    num = list(str(numbers[i]))
-    for case in num:
-        if int(case) == 3 or int(case) == 6 or int(case) == 9:
+
+    num = list(str(numbers[i]))#정수를 자릿수별로 나누기 위해 문자열로 형변환
+    if '3' in num or '6' in num or '9' in num:
             numbers[i] = 0
 print(*numbers)
