f104cb57c5ab329f62d9c8339e7702069648aa77
Polyestere <83736286+Polyestere@users.noreply.github.com>
2024-09-22 20:50:31 +0900

    Title: 청소당번 정하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240922/청소당번 정하기/cleaning-numbering.py
+++ 240922/청소당번 정하기/cleaning-numbering.py
@@ -12,5 +12,5 @@ for i in range(1,n+1):
     if i % 12 == 0:
         restroom_cnt += 1
         corridor_cnt -= 1
-
 print(class_cnt, corridor_cnt, restroom_cnt, sep=" ")
+#elif으로 12,3,2, 순서로 조건문을 통과하는 방법도 있음
