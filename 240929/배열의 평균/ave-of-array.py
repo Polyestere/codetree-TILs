d1acbbd9a1369c6e443df4f9e88a8f5c6c1ed7c8
Polyestere <83736286+Polyestere@users.noreply.github.com>
2024-09-29 18:56:30 +0900

    Title: 배열의 평균, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240929/배열의 평균/ave-of-array.py
@@ -0,0 +1,18 @@
+numbers = [list(map(int,input().split())) for _ in range(2)]
+row_mean = []
+column_mean = [0 for _ in range(4)]
+all_mean = 0
+for i in numbers:
+    row_mean.append(sum(i)/len(i))
+    all_mean += sum(i)
+for i in range(len(numbers)):
+    for j in range(len(numbers[i])):
+        column_mean[j] += numbers[i][j]
+
+for i in row_mean:
+    print('%.1f'% i,end = ' ')
+print()
+for i in column_mean:
+    print('%.1f'% (i/2),end = ' ')
+print()
+print(all_mean/8)
